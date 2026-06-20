e3d97c94b20d0458203942cfbbd9db8fea744bac
juniorgald <junior.galdbr@gmail.com>
2026-06-20 12:43:59 -0300

    feat: envia mensagens personalizadas via Z-API

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 from supabase import create_client
 import os
 
+import requests
+
 load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
@@ -14,3 +16,21 @@ response = supabase.table("contatos").select("*").execute()
 contatos = response.data
 
 print(contatos)
+
+for contato in contatos:
+    nome = contato['nome']
+    telefone = contato['telefone']
+    mensagem = f"Olá {nome} tudo bem com você?"
+
+    url = f"https://api.z-api.io/instances/{os.getenv('ZAPI_INSTANCE_ID')}/token/{os.getenv('ZAPI_TOKEN')}/send-text"
+
+    payload = {
+        "phone": telefone,
+        "message": mensagem
+    }
+
+    response = requests.post(url, json=payload)
+
+    print(f"Enviando para {nome}... Status: {response.status_code}")
+
+
