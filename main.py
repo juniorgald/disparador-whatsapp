a749409a2c8817562fa4aa72fb7b0bf4aa463e80
juniorgald <junior.galdbr@gmail.com>
2026-06-20 01:51:54 -0300

    feat: conecta ao Supabase e lê contatos

---
+++ main.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+from supabase import create_client
+import os
+
+load_dotenv()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+response = supabase.table("contatos").select("*").execute()
+
+contatos = response.data
+
+print(contatos)
